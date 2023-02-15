5f0e62e43d37d96e691352a1bf990125ebff26dd
achyut vardhan <achyutvardhan234@gmail.com>
2023-02-15 08:37:53 +0530

    ..

--- login-jwt/src/pages/Login.jsx
+++ login-jwt/src/pages/Login.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import LoginCss from './css/Login.css'
 export default function Login() {
     console.log("first")
   return (
     <>
-    <div>login</div>
+    <div className={LoginCss.loginextdiv}>
+        <div className={LoginCss.loginIntdiv}>
+            <form className={LoginCss.form}>
+            <label>
+                    Username : <input type="text" name="usern" id="usern" />
+                </label>
+                <label>
+                    password : <input type="password" name="usern" id="userp" />
+                </label>
+                <input type="submit" value="Login" />
+            </form>
+        </div>
+    </div>
     </>
   )
 }
