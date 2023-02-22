ca7cd21e0ffb76df337448c22460284bddad23dc
achyut vardhan <achyutvardhan234@gmail.com>
2023-02-22 16:40:38 +0530

    ..

--- api/server.js
+++ api/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/hello', (req, res) => {
-  res.send('hello ');
-});
-app.get('*', (req, res) => {
-  res.send('page does not exist ');
-});
-app.listen(8080, () => {
-  console.log('welcome to port 8080');
-});
+    res.send('hello world');
+  });
+  app.get('*', (req, res) => {
+    res.send('page does not exist ');
+  });
+  app.listen(8080, () => {
+    console.log('welcome to port 8080');
+  });
